4dc20f1adda275bb686a186b5c573439fce8501c
Sem Meijer <s.r.meijer.1@student.rug.nl>
2024-09-20 12:24:19 +0200

    uupdate

--- r-scripts/12-dates-times.R
+++ r-scripts/12-dates-times.R
@@ -31,14 +31,44 @@ dat_month<-dat %>%
 tail(dat_month,20) # note the definition of the metyear_nov and metyear_dec variables, these months "belong" to the next year
 
 # plot monthly average temperature with 5-year running average
-dat_month %>% ggplot(aes(x=mdate,y=avgtemp_mo_oC)) +
+dat_month |> 
+  ggplot(aes(x=mdate,y=avgtemp_mo_oC)) +
+  geom_line() + tidyquant::geom_ma(ma_fun=SMA,n=61,col="red",linetype="solid",) +
+  coord_x_date(xlim=c("2001-01-01","2024-08-31")) + # zoom to this
+  ggtitle("Monthly temperature at Lauwersoog") +
+  theme(text = element_text(size=10))
 
+      
+dat_month2 <- dat_month |>
+  dplyr::mutate(rain_SMA_13 = rollapply(totrain_mo_mm, width=13, FUN=sum, align = "right", fill = NA))
 # plot monthly rainfall with 1-year running average
-dat_month %>% ggplot(aes(x=mdate,y=totrain_mo_mm)) +
+dat_month |> ggplot() +
+  geom_line(aes(x=mdate,y=totrain_mo_mm)) + 
+  geom_line(data=dat_month2,aes(x=mdate,y=rain_SMA_13),col="red") 
+  
+ 
+dat_month |> 
+  ggplot(aes(x=mdate,y=totrain_mo_mm)) +
+  geom_line() +
+ tidyquant::geom_ma(ma_fun=SMA,n=13,col="red",linetype="solid") +
+  coord_x_date(xlim=c("2014-01-01","2024-08-31")) + # zoom to this
+  ggtitle("Monthly rainfall at Lauwersoog") +
+  theme(text = element_text(size=10)) +
+  scale_x_date(date_labels = "%Y",date_breaks = "1 year")  # show only years on x-axis 
+  
 
 # additive time series decomposition of the monthly mean temperatures: what is seasonal and what is a longterm trend?
+monthly_temps <- dat_month |>
+  dplyr::ungroup() |> 
+  dplyr::filter(year!=2024) |>
+  dplyr::select(avgtemp_mo_oC)
+plot(monthly_temps$avgtemp_mo_oC)
 
-  
+monthly_temps_ts <- ts(monthly_temps,frequency=12,start=c(1992,1))
+plot(monthly_temps_ts)
+monthly_temps_decomp <- decompose(monthly_temps_ts)
+plot(monthly_temps_decomp)
+
 # note the trend component is the same as the 12 month running average from the previous plot
 # adjust the time series for the seasonal component, and plot it
 
@@ -50,17 +80,54 @@ dat_month %>% ggplot(aes(x=mdate,y=totrain_mo_mm)) +
 
 
 # average temperature of the coldest month per meteorological year
-
+dat_metyear_dec <- dat_month |>
+  dplyr::group_by(metyear_dec) |>
+  dplyr::summarise(minmonth=min(avgtemp_mo_oC,na.rm=T))
   # note that 2010 and 2011 were the last winters with a (on average) sub-zero month, 
 # potentially relevant for cockle recruitment (crabs stay on mudflats eating spatfall)
-
+dat_metyear_dec |> ggplot(aes(x=metyear_dec,y=minmonth)) +
+  geom_line(linewidth=.7) +
+  geom_point(size=3) 
 # Hellmann index (sum of all below-zero daily average temperatures from 1 November of previous year until 31 march)
 # see https://nl.wikipedia.org/wiki/Koudegetal
-
+dat_Hellman <- dat |>
+  dplyr::filter(month %in% c(11,12,1,2,3),TG<0) |>
+  dplyr::group_by(metyear_nov) |>
+  dplyr::summarise(Hellman=sum(TG,na.rm=T))
+tail(dat_Hellman,20)
+dat_Hellman |> ggplot(aes(x=metyear_nov,y=Hellman)) +
+  geom_line(linewidth=.7) +
+  geom_point(size=3)
 # 1997 was the last "Elfstedentocht"
 # 2014 -2024 (our transect study) is characterized by only warm winters
 
 # Warmth index
+# sum of all daily average temperatures above 18.0 degrees Celsius from 1 April until 30 September
+# see https://nl.wikipedia.org/wiki/Warmtegetal
+dat_WarmthIndex <- dat |>
+  dplyr::filter(month %in% c(4:8),TG>18) |>
+  dplyr::group_by(year) |>
+  dplyr::summarise(WarmthIndex=sum(TG,na.rm=T))
+tail(dat_warmth,20)
+dat_warmth |> ggplot(aes(x=year,y=warmth)) +
+  #geom_line(linewidth=.7) +
+  geom_point(size=3) +
+  geom_smooth(method="lm")
+
+
 library(quantreg)
 # see for calculation https://www.knmi.nl/nederland-nu/klimatologie/lijsten/warmtegetallen
+#ADD THE quantile regresion lines quantreg package in this plot, 10,50, 90% quqntiles
+dat_warmth |> ggplot(aes(x=year,y=warmth)) +
+  geom_smooth(method="lm") +
+  geom_point(size=3) +
+  geom_quantile(quantiles=c(0.1,0.5,0.9),col="red",size=1)
+# note that the 90% quantile is increasing, while the 10% quantile is decreasing, 
+#indicating more extreme warm years
+# the 50% quantile is increasing, indicating a general warming trend
+
+
+
+
+
 
